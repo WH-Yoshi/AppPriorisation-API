61b2c6e167a8bd5a6b3c8a73f05a49159cbbf7a3
WH-Yoshi <absluca419@gmail.com>
2025-05-02 15:15:07 +0200

    Testing DB connection

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
+import os
+import asyncpg
 from fastapi import FastAPI
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "API OK"}
+
+@app.get("/test-db")
+async def test_db():
+    try:
+        conn = await asyncpg.connect(DATABASE_URL)
+        await conn.execute("SELECT 1")
+        await conn.close()
+        return {"status": "success", "message": "Connection to DB is OK"}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
