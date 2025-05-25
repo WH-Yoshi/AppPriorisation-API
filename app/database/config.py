3f0d8fa6b2df8f1d96d634e559eb94a230a23ab4
WH-Yoshi <absluca419@gmail.com>
2025-05-25 12:35:09 +0200

    Added admin backend

--- app/database/config.py
+++ app/database/config.py
@@ -4,17 +4,32 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-filename = os.getenv('CONFIG_FILE') or './app/database/database.ini'
+filename = os.getenv('CONFIG_FILE')
 
-def load_config(section='postgresql'):
+import os
+from configparser import ConfigParser # Assuming you are using configparser for .ini files
+
+def load_config(filename='database.ini', section='postgresql'):
     parser = ConfigParser()
     parser.read(filename)
 
-    config = {}
+    # get section, default to postgresql
+    db = {}
     if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            config[param[0]] = param[1]
-        return config
+        for item, value in parser.items(section):
+            db[item] = value
     else:
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    # --- IMPORTANT CHANGE HERE ---
+    # Try to get the password from an environment variable
+    # If not found in environment, you might consider a fallback or raise an error
+    db_password = os.getenv('POSTGRES_PASSWORD')
+    if db_password:
+        db['password'] = db_password
+    else:
+        # This is crucial: if DB_PASSWORD is not set, your app needs to know.
+        # You could raise an error, or if you had a default, use it (not recommended for prod passwords).
+        raise Exception("DB_PASSWORD environment variable not set!")
+
+    return db
